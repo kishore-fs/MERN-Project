8f10bba2c353f8a8b80dc7b37df5e69ab6967778
kishore-fs <kishoreravichandran5@gmail.com>
2023-09-24 13:32:14 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import { Home } from './home/home';
+import { Menu } from './menu/menu';
+import { Signup } from './signup/signup';
+import { Login } from './login/login';
+import { Admin } from './admin/admin';
+import { Chicken } from './product/chicken';
+import { Updatepage } from './updatepage/updatepage';
+import { Seafood } from './product/seafood';
+import { Persondetails } from './persondetails/persondetails';
+import { Mutton } from './product/mutton';
+import { Dryfood } from './product/dryfood';
+import { Egg } from './product/egg';
+import { Pickle } from './product/pickle';
+import { Orderdetails } from './orderdetails/orderdetails';
+import { Productdetails } from './productdetails/productdetails';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+         <BrowserRouter>
+    <Routes>
+      <Route path='/' element={[<Login/>]}/>
+      <Route path='/home/:id' element={[<Menu/>,<Home/>]}/>
+      <Route path='/signup' element={[<Signup/>]}/>
+      <Route path='/login' element={[<Login/>]}/>
+      <Route path='/admin/:id' element={[<Menu/>,<Admin/>]}/>
+      <Route path='/chicken/:id' element={[<Menu/>,<Chicken/>]}/>
+      <Route path='/updatepage/:id' element={[<Updatepage/>]}/>
+      <Route path='/seafood/:id' element={[<Menu/>,<Seafood/>]}/>
+      <Route path='/persondetails' element={[<Menu/>,<Persondetails/>]}/>
+      <Route path='/mutton/:id' element={[<Menu/>,<Mutton/>]}/>
+      <Route path='/dryfood/:id' element={[<Menu/>,<Dryfood/>]}/>
+      <Route path='/egg/:id' element={[<Menu/>,<Egg/>]}/>
+      <Route path='/pickle/:id' element={[<Menu/>,<Pickle/>]}/>
+      <Route path='/orderdetails' element={[<Menu/>,<Orderdetails/>]}/>
+      <Route path='/productdetails' element={[<Menu/>,<Productdetails/>]}/>
+    </Routes>
+    </BrowserRouter> 
+    </>
   );
 }
 
